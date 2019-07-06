ac859d30cdd80b7686c83b2b2bcd2330b4d7c7c6
Cliff Karlsson <ntn15cko@student.hig.se>
2019-07-06 07:23:05 +0200

    refinded unicafe and anecdotes

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -11,33 +11,15 @@ const Button = ({ onClick, text }) => {
   )
 }
 
-const Display = ({ text }) => {
-  return (
-    <h1>{text}</h1>
-  )
-}
-const History = ({ hasRating }) => {
-  if (!hasRating) {
-    return (
-      <>
-        <h1>statistics</h1>
-        <p>No feedback given</p>
-      </>
-    )
-  }
-  return (<h1>statistics</h1>)
-}
+const Statistics = ({ text, value }) => {
 
-const Statistics = ({ text, value, hasRating }) => {
-  if (hasRating) {
-    return (
-      <tr>
-        <td>{text}</td>
-        <td>{value}</td>
-      </tr>
-    )
-  }
-  return null
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+
 }
 
 const average = (good, bad, total) => {
@@ -45,7 +27,7 @@ const average = (good, bad, total) => {
 }
 
 const positive = (good, total) => {
-  return ((good / total) * 100 + " %")
+  return ((good / total) * 100)
 }
 
 const App = () => {
@@ -57,24 +39,27 @@ const App = () => {
   const hasRatings = totalRatings > 0
 
   return (
-    <div>
-      <Display text="give feedback" />
+    <>
+      <h1>give feedback</h1>
       <Button onClick={() => setGood(good + 1)} text="good" />
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
 
-      <History hasRating={hasRatings} />
-      <table>
-        <tbody>
-          <Statistics text="good" value={good} hasRating={hasRatings} />
-          <Statistics text="neutral" value={neutral} hasRating={hasRatings} />
-          <Statistics text="bad" value={bad} hasRating={hasRatings} />
-          <Statistics text="all" value={totalRatings} hasRating={hasRatings} />
-          <Statistics text="average" value={average(good, bad, totalRatings)} hasRating={hasRatings} />
-          <Statistics text="positive" value={positive(good, totalRatings)} hasRating={hasRatings} />
-        </tbody>
-      </table>
-    </div>
+      <h1>statistics</h1>
+      {hasRatings ? (
+        <table>
+          <tbody>
+            <Statistics text="good" value={good} />
+            <Statistics text="neutral" value={neutral} />
+            <Statistics text="bad" value={bad} />
+            <Statistics text="all" value={totalRatings} />
+            <Statistics text="average" value={average(good, bad, totalRatings)} />
+            <Statistics text="positive" value={positive(good, totalRatings) + " %"} />
+          </tbody>
+        </table>
+      ) : <p>No feedback</p>
+      }
+    </>
 
   )
 }
