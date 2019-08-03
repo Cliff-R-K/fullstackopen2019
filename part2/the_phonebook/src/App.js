3c9991a2776d84916325316c73ca9f9ca8586681
Cliff Karlsson <ntn15cko@student.hig.se>
2019-08-03 08:46:27 +0200

    last fixes

--- part2/the_phonebook/src/App.js
+++ part2/the_phonebook/src/App.js
@@ -8,82 +8,89 @@ import Notification from './components/Notification'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNumber] = useState('')
-  const [showFiltered, setFiltered] = useState('')
-  const [notificationMessage, setNotificationMessage] = useState({message:null, class:null})
+    const [persons, setPersons] = useState([])
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNumber] = useState('')
+    const [showFiltered, setFiltered] = useState('')
+    const [notificationMessage, setNotificationMessage] = useState({ message: null, class: null })
 
 
-  const addAll = () => {
-    phonebookService.getAll().then(storedPhonebook => { setPersons(storedPhonebook) })
-  }
-
-  useEffect(addAll, [])
-
-  const addPersonToList = (event) => {
-    event.preventDefault()
-
-    const newPerson = {
-      name: newName,
-      number: newNumber
+    const addAll = () => {
+        phonebookService.getAll().then(storedPhonebook => { setPersons(storedPhonebook) })
     }
 
-    phonebookService.create(newPerson).then(response => {
-      console.log('Response from App: ', response, 'newPerson',newPerson);
-      if (response.name !== newPerson.name) {
-        const indexOfElement = persons.findIndex(p => p.name === response.name)
-        console.log('Before', persons, 'Index', indexOfElement);
-        persons[indexOfElement].number = response.number
-      }
-      else{
-        persons.push(response)
-      }
-      console.log('after', persons);
-      setPersons(persons)
-      setNewName('')
-      setNumber('')
-      setNotificationMessage({message:`Added ${response.name}`, class:`notification`})
-      console.log(notificationMessage)
-      setTimeout(() => {
-        setNotificationMessage({message:null, class:null})
-      }, 5000)
+    useEffect(addAll, [])
 
-    })
-  }
+    const addPersonToList = (event) => {
+        event.preventDefault()
 
-  const removePerson = person => {
-    const confirmation = window.confirm(`delete ${person.name} ?`)
-    console.log('Confirmation', confirmation)
-    if (confirmation) {
-      phonebookService.remove(person.id)
-      setPersons(persons.filter(p => p.id !== person.id))
+        const newPerson = {
+            name: newName,
+            number: newNumber
+        }
+
+        phonebookService.create(newPerson).then(response => {
+            console.log('Response from App: ', response, 'newPerson', newPerson);
+            if (response) {
+                addAll()
+                console.log('after', persons);
+
+                setNotificationMessage({ message: `Added ${response.name}`, class: `notification` })
+                console.log(notificationMessage)
+                setTimeout(() => {
+                    setNotificationMessage({ message: null, class: null })
+                }, 5000)
+            }
+            setNewName('')
+            setNumber('')
+
+
+        })
     }
-  }
+
+    const removePerson = person => {
+        const confirmation = window.confirm(`delete ${person.name} ?`)
+        console.log('Confirmation', confirmation)
+        if (confirmation) {
+            phonebookService.remove(person.id)
+            setPersons(persons.filter(p => p.id !== person.id))
+        }
+    }
 
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
-    setNumber(event.target.value)
-  }
-  const handleFilterChange = (event) => {
-    setFiltered(event.target.value)
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+    const handleNumberChange = (event) => {
+        setNumber(event.target.value)
+    }
+    const handleFilterChange = (event) => {
+        setFiltered(event.target.value)
 
-  }
+    }
 
-  return (
-    <div>
-      <h1 id='Header'>Phonebook</h1>
-      <Notification message={notificationMessage.message} className={notificationMessage.class}/>
-      <Filter value={showFiltered} onChange={handleFilterChange} />
-      <h3>add a new</h3>
-      <PersonForm addPerson={addPersonToList} newName={newName} nameHandler={handleNameChange} newNumber={newNumber} numberHandler={handleNumberChange} />
-      <h3>Numbers</h3>
-      <Persons filter={showFiltered} persons={persons} removePerson={removePerson} />
+    return (<div>
+        <h1 id='Header'> Phonebook </h1>
+        <Notification message={notificationMessage.message}
+            className={notificationMessage.class}
+        />
+        <Filter value={showFiltered}
+            onChange={handleFilterChange}
+        />
+        <h3 > add a new </h3>
+        <PersonForm addPerson={addPersonToList}
+            newName={newName}
+            nameHandler={handleNameChange}
+            newNumber={newNumber}
+            numberHandler={handleNumberChange}
+        />
+        <h3 > Numbers </h3>
+        <Persons filter={showFiltered}
+            persons={persons}
+            removePerson={removePerson}
+        />
     </div >
-  )
+    )
 }
 
 export default App
